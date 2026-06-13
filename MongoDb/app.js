7a317f7a9acdcba2702eba6ec5a20245cd5b210b
Garuda-99 <165791347+Garuda-99@users.noreply.github.com>
2026-06-13 18:23:59 +0530

    Add .gitignore

---
+++ MongoDb/app.js
@@ -0,0 +1,70 @@
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(
+    "mongodb+srv://cosmicsaitama777_db_user:1jkXrxgykQcYKoZN@cluster0.tbchrjn.mongodb.net/",
+  )
+  .then(() => {
+    console.log("Connected to database");
+    runQuery();
+  })
+  .catch((err) => {
+    console.log("Error connecting to database", err);
+  });
+
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  age: Number,
+  isActive: Boolean,
+  tags: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+
+async function runQuery() {
+  try {
+    const newUser = await User.create({
+      name: "gta 6",
+      email: "huiy@example.com",
+      age: 28,
+      isActive: true,
+      tags: ["tag.1", "tag.2"],
+    });
+
+    await newUser.save();
+
+    console.log("User saved successfully");
+    console.log(newUser);
+    const AllUsers = await User.find();
+    console.log("All Users:", AllUsers);
+    const selectedFields = await User.find().select("name email -_id");
+    console.log("Selected Fields:", selectedFields);
+    const limitedUsers = await User.find().limit(3).skip(1);
+    console.log("Limited Users:", limitedUsers);
+    const sortedUsers = await User.find().sort({ age: 1 });
+    console.log("Sorted Users:", sortedUsers);
+    const countDocuments = await User.countDocuments({ isActive: true });
+    console.log("Count of Active Users:", countDocuments);
+
+    const updatedUser = await User.findByIdAndUpdate(
+      newUser._id,
+      {
+        $set: { age: 30 },
+        $push: { tags: "updated" },
+      },
+      { new: true },
+    );
+    console.log("Updated User:", updatedUser);
+    // const dltUser = await User.findByIdAndDelete(newUser._id);
+    // console.log("Deleted User:", dltUser);
+  } catch (err) {
+    console.log("Error:", err);
+  } finally {
+    await mongoose.connection.close();
+  }
+}
--- restApi/app.js
+++ restApi/app.js
@@ -4,6 +4,7 @@ app.use(express.json());
 let books = [
   { id: 1, name: "book1", author: "author1" },
   { id: 2, name: "book2", author: "author2" },
+  { id: 3, name: "book3", author: "author3" },
 ];
 app.get("/", (req, res) => {
   res.send("tatakae");
@@ -24,6 +25,26 @@ app.get("/get/:id", (req, res) => {
   const jui = books.find((book) => book.id === parseInt(req.params.id));
   if (jui) {
     res.status(200).json(jui);
+  } else {
+    res.status(404).json({ message: "book not found" });
+  }
+});
+app.put("/update/:id", (req, res) => {
+  const findra = books.find((book) => book.id === parseInt(req.params.id));
+  if (findra) {
+    findra.title = req.body.title || findra.title;
+    res
+      .status(200)
+      .json({ data: findra, message: "book updated successfully" });
+  } else {
+    res.status(404).json({ message: "book not found" });
+  }
+});
+app.delete("/delete/:id", (req, res) => {
+  const findra = books.find((book) => book.id === parseInt(req.params.id));
+  if (findra) {
+    books = books.filter((book) => book.id !== parseInt(req.params.id));
+    res.status(200).json({ message: "book deleted successfully" });
   } else {
     res.status(404).json({ message: "book not found" });
   }
